3fd8af812c6070fa1ee60e4e7060adcfa491b4ba
João Pedro <albuquerquejoaopedro04@gmail.com>
2025-04-06 21:24:50 -0300

    refactor: set navbar to transparent in home view

--- client/src/views/Home/HomeContainer.tsx
+++ client/src/views/Home/HomeContainer.tsx
@@ -1,6 +1,14 @@
+import { useNavbarStore } from "@/store/useNavbarStore";
 import Home from "./Home";
+import { useEffect } from "react";
 
 const HomeContainer = () => {
+  const { setVariant } = useNavbarStore();
+
+  useEffect(() => {
+    setVariant("transparent");
+  }, [setVariant]);
+
   return <Home />;
 };
 
